306861d11e17ed4e766597868d91b4b6a951de5c
Andre <andreluis_57@hotmail.com>
2026-02-03 16:45:18 -0300

    ajuste

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,21 @@ const config: CapacitorConfig = {
   plugins: {
     AdMob: {
       appId: 'ca-app-pub-1110010077986201~4016518930'
+    },
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_INSIDE",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: false
     }
   }
 };
